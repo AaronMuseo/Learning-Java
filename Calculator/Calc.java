5053bb6ae6ef0c4d506e94305588bb7ad388de6e
Aaron Museo Nzangi <132920661+AaronMuseo@users.noreply.github.com>
2025-06-14 14:55:48 +0300

    Create Calc.java
    Added a folder for calculator project and added Calc.java

---
+++ Calculator/Calc.java
@@ -0,0 +1,32 @@
+package Calculator;
+
+public class Calc {
+
+    public float addition(float num1, float num2){
+
+        float sum = num1 + num2;
+        return sum;
+
+    }
+
+    public float subtraction(float num1, float num2){
+
+        float sub = num1 - num2;
+        return sub;
+
+    }
+
+    public float division(float num1, float num2){
+
+        float div = num1/num2;
+        return div;
+
+    }
+
+    public float multiplication(float num1, float num2){
+        float product = num1 * num2;
+        return product;
+
+
+    }
+}
